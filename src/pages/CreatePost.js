e716eacfd6408efbadd9117c92836fea81c41ab4
YeshwanthSaiSrinivas <yeshwanthsaisrinivas.v20@iiits.in>
2024-01-18 00:13:31 +0530

    content type error

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -34,6 +34,9 @@ export default function CreatePost() {
         method: 'POST',
         body: data,
         credentials: 'include',
+        headers:{
+          'Content-Type': 'multipart/form-data'
+        }
       });
       if (response.ok) {
         setRedirect(true);
